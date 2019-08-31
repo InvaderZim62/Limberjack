c94e72e2bde42de491a69dc70075758549a1c21c
Invader Zim <pwstern@gmail.com>
2019-08-30 20:31:24 -0700

    Created a barView with attachmentBehavior to rotate about its end point when colliding with blockView that is pushed into it.

--- Limberjack/LimberjackViewController.swift
+++ Limberjack/LimberjackViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import CoreMotion
+
+struct Constants {
+    static let attachmentPoint = CGPoint(x: 200, y: 250)  // relative to animator's reference view
+    static let viewWidth = CGFloat(4)
+    static let viewHeight = CGFloat(150)
+}
 
 class LimberjackViewController: UIViewController {
+    
+    let barView = UIView(frame: CGRect(x: Constants.attachmentPoint.x - Constants.viewWidth / 2,
+                                       y: Constants.attachmentPoint.y,
+                                       width: Constants.viewWidth,
+                                       height: Constants.viewHeight))
+    
+    let blockView = UIView(frame: CGRect(x: 50, y: 320, width: 20, height: 20))
+    
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    
+    lazy var collisionBehavior: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(behavior)
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = true
+        behavior.elasticity = 0.7
+        behavior.resistance = 0
+        animator.addBehavior(behavior)
+        return behavior
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        blockView.backgroundColor = .black
+        view.addSubview(blockView)
+        
+        barView.backgroundColor = .red
+        view.addSubview(barView)
+        
+        collisionBehavior.addItem(barView)
+        collisionBehavior.addItem(blockView)
+        itemBehavior.addItem(barView)
 
+        let attachment = UIAttachmentBehavior(item: barView,
+                                              offsetFromCenter: UIOffset(horizontal: 0, vertical: -Constants.viewHeight / 2),
+                                              attachedToAnchor: Constants.attachmentPoint)
+        animator.addBehavior(attachment)
+        
+        let push = UIPushBehavior(items: [blockView], mode: .instantaneous)
+        push.angle = 0
+        push.magnitude = 0.1
+        push.action = { [unowned push] in
+            push.dynamicAnimator?.removeBehavior(push)
+        }
+        animator.addBehavior(push)
+    }
 }
 
